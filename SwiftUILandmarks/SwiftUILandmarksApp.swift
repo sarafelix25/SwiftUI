dc830e69cf765a7d2f8f99d33a48b76dbff99972
sarafelix <x283609@prservicos.com.br>
2022-02-21 12:13:55 -0300

    Initial Commit

---
+++ SwiftUILandmarks/SwiftUILandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILandmarksApp.swift
+//  SwiftUILandmarks
+//
+//  Created by Sara Batista dos Santos Felix (P) on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
